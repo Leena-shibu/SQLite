de5ee12f6115c1a43fb40c8768823ffe310d493c
Leena-shibu <44475668+Leena-shibu@users.noreply.github.com>
2021-11-07 20:57:20 +0530

    Add files via upload

---
+++ titanic.sql
@@ -0,0 +1,14 @@
+select * from titanic;
+
+--1.Display all the rows and columns from table titanic
+--2.Write a query to display first_name,survived,age.
+--3.Write a query to display first_name,age,class where survived is 1
+--4.Write a query to display first_name,pclass ,embark_town where survived=1 and embark_town='Southampton'
+--5.Write a query to display first_name,sex where fare between 20,000 and 50,000
+--6.Write a query to display all the passenger details aged between 20 and 50 
+--7.Write a query to find the details of all passengers who are female and are aged between 0 to 20.
+--8.Write a query to find first_name,age and class who are in First Class and 'who' is 'woman'
+--9.Write a query to find first_name,age and survived who has 'o' in second position
+--10. Write a query to display concat first_name,last_name and display in following format
+'Steven King survived Titanic'
+--11.Write a query to display all the details in the descending order of fare 
