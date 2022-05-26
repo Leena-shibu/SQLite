8f89da2324751f92e96f1dc29a03459d800ce8e5
Leena-shibu <44475668+Leena-shibu@users.noreply.github.com>
2022-05-26 13:21:49 +0530

    Add files via upload

---
+++ Tiger/Day2./cereal_groupby_questions.sql
@@ -0,0 +1,41 @@
+select * from cereal;
+-- Group by Queries on cereal.csv
+--1.Write A Query display count of cereals mfr wise
+
+
+--2.Write A Query to display count of cereals type wise
+
+
+--3.Write A Query to display average  of carbo cereals mfr wise
+
+
+--4.Write A Query to display sum of rating  of  cereals mfr wise
+
+
+----5.Write A Query to display average of protein  of  cereals mfr wise
+
+
+----6. Write A Query to display sum of weight  of  cereals type wise
+
+--start up
+--
+Naukri --3 years 
+female gender same qualification -salary less
+male gender --
+ML-ENgineer
+Data Scientist --
+Data Engineer --.csv,.json,.doc ,500 attributes 
+Data model --
+Data Analyst-- seaborn,Tableau ,PowerBi ,
+Data collecting ---clean ,process
+
+--7.Write A Query to display max(potass),max(vitamins),max(rating)  of  cereals type wise
+
+--8. Display cereal name having highest sugar.
+-- Cereals not healthy--
+
+-- Highest carbs 
+-- cereal more calories 
+-- fiber rich 
+--Joins 
+
